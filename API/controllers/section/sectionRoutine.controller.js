95ebb882a2d36fe08744238c189fdd16db08568b
MM-Mamunne <gamesmamun8@gmail.com>
2024-10-17 20:25:29 +0600

    count added for colspan

--- API/controllers/section/sectionRoutine.controller.js
+++ API/controllers/section/sectionRoutine.controller.js
@@ -12,8 +12,20 @@ const sectionRoutine = async (req, res) => {
       "SELECT * FROM class WHERE sec = $1 order by day , slot",
       [section]
     );
-
-    return res.status(200).json({ rows: user.rows });
+    let final = []
+    for ( let i = 0; i < user.rows.length; i++)
+    {
+      let temp = user.rows[i];
+      let cnt = 1;
+      while( (i + 1) < user.rows.length && user.rows[i + 1].code == temp.code && user.rows[i + 1].room == temp.room && user.rows[i + 1].faculty == temp.faculty  )
+      {
+        ++i;
+        ++cnt;
+      }
+      let temp2 = {...temp,"count" : cnt};
+      final.push(temp2);
+    }
+    return res.status(200).json({ rows: final });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
