c6b9298af9f040d9800298592a522d3b7dd0c2c9
MM-Mamunne <gamesmamun8@gmail.com>
2024-12-16 12:46:26 +0600

    Class room availability based on Building

--- API/controllers/Room.controller.js
+++ API/controllers/Room.controller.js
@@ -1,36 +1,60 @@
-
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import pool from '../db.js';
+import pool from "../db.js";
 
+const RoomAvailability = async (req, res) => {
+  const { day, slot, building } = req.params;
 
-const RoomAvailability =async (req,res)=> {
-  // const { day, slot } = req.body;
-  
-  const day = req.params.slug1 ;
-  const slot = req.params.slug2;
-  // console.log("login attempted");
-  // console.log(id,password);
-  console.log("ROom check requested",day,slot);
-  
+  console.log("ROom check requested", day, slot);
+
   try {
-    if( !slot || day < 0 || day > 6 || slot < 1 || slot > 6) 
-        return res.status(401).json("Invalid Credential ");
-    const room =await pool.query(`select room from (
-	select room, coalesce(code,'specialType') as code  from classroom natural left outer join 
-	  (select * from class where day = $1 and slot = $2) as t1
-    ) as t2 where code ='specialType' ` , [
-        day, slot
-        ]);
-        console.log("data ",room.rows);
-        
-        res.json(room.rows)
+    if (!slot || day < 0 || day > 6 || slot < 1 || slot > 6)
+      return res.status(401).json("Invalid Credential ");
+    if (!building) {
+      const room = await pool.query(
+        `select room from (
+    select room, coalesce(code,'specialType') as code  from classroom natural left outer join 
+      (select * from class where day = $1 and slot = $2) as t1
+      ) as t2 where code ='specialType' `,
+        [day, slot]
+      );
+      console.log(room.rows);
+      res.json(room.rows);
+    } 
+    
+    else {
+      const rooms = await pool.query(
+        `select room from (
+    select room, coalesce(code,'specialType') as code  from classroom natural left outer join 
+      (select * from class where day = $1 and slot = $2) as t1
+      ) as t2 where code ='specialType' `,
+        [day, slot]
+      );
+      // console.log(rooms.rows);
+      
+      if (Array.isArray(rooms.rows)) {        
+        if (building == "CX" ) {          
 
+            rooms.rows = rooms.rows.filter(row => row.room.includes(building) && !row.room.includes('CXB'));
+        } 
+        else if (building == "CXB" ) {          
+
+            rooms.rows = rooms.rows.filter(row => row.room.includes(building) );
+        } 
+        else {
+          rooms.rows = rooms.rows.filter(row => !row.room.includes('CXB') && !row.room.includes('CX'));
+        }
+        res.json(rooms.rows); // Send the filtered data
+    } else {
+        // Handle the case where res.rows is not defined or not an array
+        res.json({ error: "No rows to process." });
+    }
+
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
   }
 };
 
-
-export { RoomAvailability};
+export { RoomAvailability };
--- API/routes/Room.route.js
+++ API/routes/Room.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import {RoomAvailability} from '../controllers/Room.controller.js'
 
-router.get('/available/:slug1/:slug2',RoomAvailability);
+router.get('/available/:day/:slot/:building?',RoomAvailability);
 
 
 export default router;
